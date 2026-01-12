d97fef39399b0a1bc82d55f35ad5e077e020c2d9
jaikaran109 <jaik05581@gmail.com>
2026-01-12 12:10:54 +0530

    Create 231A_Team.java

---
+++ 231A_Team.java
@@ -0,0 +1,44 @@
+package CodeForces;
+
+
+//One day three best friends Petya, Vasya and Tonya decided to form a team and take part in programming contests. Participants are usually
+//offered several problems during programming contests. Long before the start the friends decided that they will implement a problem if at
+//least two of them are sure about the solution. Otherwise, the friends won't write the problem's solution.
+//
+//This contest offers n problems to the participants. For each problem we know, which friend is sure about the solution. Help the friends find
+//the number of problems for which they will write a solution.
+//
+//Input
+//The first input line contains a single integer n (1 < n ≤ 1000) - the number of problems in the contest. Then n lines contain three integers
+//each, each integer is either 0 or 1. If the first number in the line equals 1, then Petya is sure about the problem's solution, otherwise he isn't
+//sure. The second number shows Vasya's view on the solution, the third number shows Tonya's view. The numbers on the lines are
+//separated by spaces.
+//
+//Output
+//Print a single integer - the number of problems the friends will implement on the contest.
+
+
+import java.util.*;
+public class Team {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		int[] arr = new int[3];
+		int ans = 0;
+		while(n > 0) {
+			int count = 0;
+			for(int i = 0 ; i < 3 ; i++) {
+				arr[i] = input.nextInt();
+				if(arr[i] == 1)
+					count++;
+			}
+			if(count >= 2)
+				ans++;
+			n--;
+		}
+		System.out.print(ans);
+
+	}
+
+}
