946728a81fe4bfa419a556a6662a9377c9f3bb21
jaikaran109 <jaik05581@gmail.com>
2026-01-13 10:57:07 +0530

    Create A50_Domino_Piling.java

---
+++ A50_Domino_Piling.java
@@ -0,0 +1,33 @@
+package CodeForces;
+
+
+//You are given a rectangular board of M x N squares. Also you are given an unlimited number of standard domino pieces of 2 x 1 squares.
+//You are allowed to rotate the pieces. You are asked to place as many dominoes as possible on the board so as to meet the following
+//conditions:
+//
+//1. Each domino completely covers two squares.
+//
+//2. No two dominoes overlap.
+//
+//3. Each domino lies entirely inside the board. It is allowed to touch the edges of the board.
+//
+//Find the maximum number of dominoes, which can be placed under these restrictions.
+//
+//Input
+//In a single line you are given two integers M and N - board sizes in squares (1≤M≤N ≤ 16).
+//
+//Output
+//Output one number - the maximal number of dominoes, which can be placed.
+
+
+import java.util.*;
+public class A50_Domino_Piling {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		int m = input.nextInt();
+		System.out.print((m*n) / 2);
+	}
+
+}
