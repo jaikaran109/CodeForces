1b8fe2ff44d9bd0bfe90fbfc1bc2ff5d5c0bf619
jaikaran109 <jaik05581@gmail.com>
2026-06-03 02:31:05 +0530

    Update 339A_HelpfulMaths.java

--- 339A_HelpfulMaths.java
+++ 339A_HelpfulMaths.java
@@ -1,1 +1,58 @@
+package CodeForces;
 
+
+
+//Xenia the beginner mathematician is a third year student at elementary school. She is now
+//learning the addition operation.
+//
+//The teacher has written down the sum of multiple numbers. Pupils should calculate the sum.
+//To make the calculation easier, the sum only contains numbers 1, 2 and 3. Still, that isn't
+//enough for Xenia. She is only beginning to count, so she can calculate a sum only if the
+//summands follow in non-decreasing order. For example, she can't calculate sum 1+3+2+1 but
+//she can calculate sums 1+1+2 and 3+3.
+//
+//You've got the sum that was written on the board. Rearrange the summans and print the sum
+//in such a way that Xenia can calculate the sum.
+//
+//Input
+//The first line contains a non-empty string s - the sum Xenia needs to count. String s contains
+//no spaces. It only contains digits and characters "+". Besides, string s is a correct sum of
+//numbers 1, 2 and 3. String s is at most 100 characters long.
+//
+//Output
+//Print the new sum that Xenia can count.
+//
+//Examples
+//input
+//3+2+1
+//
+//output
+//1+2+3
+
+//input
+//1+1+3+1+3
+//
+//output
+//1+1+1+3+3
+
+
+import java.util.*;
+
+public class HelpfulMaths {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String str = input.nextLine();
+
+        String[] arr = str.split("\\+");
+
+        Arrays.sort(arr);
+
+        System.out.print(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            System.out.print("+" + arr[i]);
+        }
+    }
+}
